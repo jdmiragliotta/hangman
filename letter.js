09bcc060068dcd71d1e6f86c2dce8bccc40b7ef4
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-23 15:13:34 -0500

    Created letter.js and constructor function

---
+++ letter.js
@@ -0,0 +1,15 @@
+function Letter(let){
+  this.charac = let,
+  this.appear = false,
+  this.letterRender = function(){
+    if (this.appear === false){
+      return "_";
+    }else{
+      return this.charac;
+    }
+  }
+}
+
+letterRender();
+
+module.exports = Letter;
