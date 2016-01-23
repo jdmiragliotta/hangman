b95ce500f9225fd5bd0b3496a8478b91db33a164
Jeremy Miragliotta <jdmiragliotta@gmail.com>
2016-01-23 17:22:40 -0500

    cleaned code and errors

--- main.js
+++ main.js
@@ -3,40 +3,40 @@ prompt.start();
 var Word = require("./word.js");
 
 var game = {
-  wordBank : ["array", "boolean", "comments", "conditionals", "console", "functions", "loops", "math", "numbers", "objects", "strings" "variable"],
+  wordBank :["array", "boolean", "string", "function", "error", "object", "conditional", "comments", "console"],
   guessesRemaining : 10,
   currentWrd : null,
-  startGame : function(wrd){
-    num = Math.floor(Math.random() * (12 - 1) + 1)
-    var word = new Word(ths.wordBank[num]);
+  startGame : function(){
+    num = Math.floor(Math.random() * (9 - 1) + 1)
+    var word = new Word(this.wordBank[num]);
     this.currentWrd = word;
     this.currentWrd.getLets();
     this.keepPromptingUser();
   },
-  function keepPromptingUser(){
+  keepPromptingUser : function(){
     var self = this;
     prompt.get(["guessLetter"], function(err, result){
-      console.log("THe Letter of space you guess is "+ result.guessLetter);
-      var findHowManyofUserGuess = currentWrd.checkIfLetterFound(result.guessLetter);
+      console.log("The letter or space you guessed is " + result.guessLetter);
+      var findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter);
       if (findHowManyOfUserGuess === 0){
-        console.log("You guessed wrong!")
-        self.guessesRemaining -1;
+        console.log("You guessed wrong!");
+        self.guessesRemaining -= 1;
       }else{
-        console.log("You guessed right!");
-        if(self.currentWrd.didWeFindTheWord()=== true){
-          console.log("You Won!");
+        console.log("You guessed right!")
+        if (self.currentWrd.didWeFindTheWord() === true){
+          console.log("You Won!!");
           return 1;
         }else{
-          console.log("Guesses Remaining: "+ self.guessesRemaining);
+          console.log("guessesRemaining:" + self.guessesRemaining);
           console.log(self.currentWrd.wordRender());
         }
       }
       if (self.guessesRemaining > 0 && self.currentWrd.found === false){
-        self/keepPromptingUser();
+        self.keepPromptingUser();
       }
-      else if(self.guessesRemaining ===0){
-        console.log("Game over Bro");
-        console.log("The workd was " + self.currentWrd.word);
+      else if(self.guessesRemaining === 0){
+        console.log("Game over bro");
+        console.log("The word was " + self.currentWrd.word);
       }else{
         console.log(self.currentWrd.wordRender());
       }
--- word.js
+++ word.js
@@ -1,38 +1,39 @@
 var Letter = require("./letter.js");
 
-function Word(wrd){
+var Word = function(wrd){
   this.word = wrd;
   this.lets = [];
   this.found = false;
   this.getLets = function(){
-    for(var i = 0; i < word.length; i++){
-      word.split(' ');
-      this.lets = Letter(this.word[i]);
-      console.log("lets = " + this.lets); 
-    }; 
-  }// end getLets
+    for (var i = 0; i < this.word.length; i++){
+      letter = this.word.charAt(i);
+      var let = new Letter(letter);
+      this.lets.push(let);
+    }
+  }
   this.checkIfLetterFound = function(guessLetter){
-    var whatToReturn = 0;
-    for(var i = 0; i < this.lets.length; i++){
-      if(this.lets[i].charac === guessLetter){
+    whatToReturn = 0;
+    for (var i = 0; i < this.lets.length; i++){
+      if (this.lets[i].charac === guessLetter){
         this.lets[i].appear = true;
-        whatToReturn++;       
+        whatToReturn += 1;
       }
     }
-    return whatToReturn;
-  }//end checkIfLetterFound
+    return whatToReturn
+  }
   this.didWeFindTheWord = function(){
-    if(this.lets.every(function(curLet){
+    if (this.lets.every(function(curLet){
       return curLet.appear === true
       }) === true){
+      return true;
     }else{
 
     }
-  }//end didWeFindTheWord
+  }
   this.wordRender = function(){
     var str = "";
-    for(var i = 0; i < this.lets.length; i++){
-      str += this.let[i].letterRender(this.lets[i])
+    for (var i = 0; i < this.lets.length; i++){
+      str += this.lets[i].letterRender(this.lets[i])
     }
     return str;
   }
